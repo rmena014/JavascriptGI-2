d510e63f22bc68a23350445d28f73be7c5961efa
rmena014 <rmena014@gmail.com>
2022-09-26 13:24:25 -0400

    Move files into folders

--- easy.js
+++
@@ -1,9 +0,0 @@
-const student = [
-  { lastName: "Mena", firstName: "Richard", age: 21 },
-  { lastName: "Viruega", firstName: "Jennifer", age: 22 },
-  { lastName: "Estevez", firstName: "Luis", age: 28 },
-];
-
-console.log(
-  `Hello, my name is ${student[1].firstName} ${student[1].lastName} and I am ${student[1].age} years old!`
-);
