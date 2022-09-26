88cf636adbbccfa5c1318e2d9150344ebbaad868
rmena014 <rmena014@gmail.com>
2022-09-26 16:57:01 -0400

    Very Easy

--- VeryEasy/veryEasy.js
+++ VeryEasy/veryEasy.js
@@ -1,7 +1,7 @@
-function min(x, y, z) {
-  return Math.min(x, y, z);
+function min(x, y) {
+  return Math.min(x, y);
 }
 
-console.log(min(10, 23, 5));
-console.log(min(254, 213, 105));
-console.log(min(24, 25, 26));
+console.log(min(10, 23));
+console.log(min(254, 213));
+console.log(min(24, 25));
